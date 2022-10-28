4b6ff246c7653e6a1b57f8f4dbe5c2f443862d0b
mg <amg.vergara@gmail.com>
2022-10-28 02:09:06 -0300

    Se modifica renderTareas para recibir objetoTareas como parametro. Se agregan las 3 tareas iniciales

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,27 +3,35 @@ const tareaInput = document.querySelector('#nuevaTarea')
 const btnAgregar = document.querySelector('#agregarTarea')
 const cuentaTareas = document.querySelector('#cuenta-tareas')
 
-const tareas = []
+const tareas = [
+  { id: Date.now(), nombre: 'Pasear al perro', completado: false },
+  { id: Date.now(), nombre: 'Estudiar JS', completado: false },
+  { id: Date.now(), nombre: 'Estudiar python', completado: false }
+]
+
+renderTareas(tareas)
+
+// const tareas = []
 btnAgregar.addEventListener('click', () => {
   console.log('entra')
   /* Agregamos el invitado al arreglo */
   const nombreTarea = tareaInput.value
-  tareas.push({ id: Date.now(), nombre: nombreTarea })
+  tareas.push({ id: Date.now(), nombre: nombreTarea, completado: false })
   tareaInput.value = ''
   /* Actualizamos la información en el HTML */
-  renderInvitados()
+  renderTareas(tareas)
 })
 
 function borrar (id) {
   const index = tareas.findIndex((ele) => ele.id === id) /* 2.1 */
   tareas.splice(index, 1) /* 2.2 */
-  renderInvitados()
+  renderTareas(tareas)
 }
 
 /* Actualizamos la información en el HTML */
-function renderInvitados () {
+function renderTareas (objetoTareas) {
   let html = ''
-  for (const value of tareas) {
+  for (const value of objetoTareas) {
     html += `<ul class="list-group list-group-horizontal row">
               <li class="list-group-item col">${value.id} </li>
               <li class="list-group-item col">${value.nombre} </li>
@@ -32,6 +40,7 @@ function renderInvitados () {
                   <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
                     <label class="form-check-label" for="flexCheckDefault">
                       Tarea terminada
+                      ${value.completado}
                     </label>
                   </div>
               </li>
@@ -39,9 +48,7 @@ function renderInvitados () {
                <button onclick="borrar(${value.id})" class="btn btn-danger" type="button" id="button-addon2">Eliminar</button>
               </li>
             </ul>`
-    // `<li>${value.nombre} <input type="checkbox" id="cbox1" value="first_checkbox"> <button
-    // onclick="borrar(${value.id})"> x </button> </li>`
   }
   listaDeTareas.innerHTML = html
-  cuentaTareas.innerHTML = tareas.length
+  cuentaTareas.innerHTML = objetoTareas.length
 }
