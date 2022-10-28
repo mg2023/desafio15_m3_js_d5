ab6ca73758e726f32014847bfe9c219fcb02c3bf
mg <amg.vergara@gmail.com>
2022-10-28 10:58:29 -0300

    Se agrega el contador de tareas terminadas

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,12 @@ const listaDeTareas = document.querySelector('#tareas')
 const tareaInput = document.querySelector('#nuevaTarea')
 const btnAgregar = document.querySelector('#agregarTarea')
 const cuentaTareas = document.querySelector('#cuenta-tareas')
+const idTareasTerminadas = document.querySelector('#tareas-terminadas')
 
 const tareas = [
-  { id: Date.now(), nombre: 'Pasear al perro', completado: false },
-  { id: Date.now(), nombre: 'Estudiar JS', completado: false },
-  { id: Date.now(), nombre: 'Estudiar python', completado: false }
+  { id: 1, nombre: 'Pasear al perro', completado: 'Finalizar', colorBotonCompletado: 'btn-primary' },
+  { id: 2, nombre: 'Estudiar JS', completado: 'Finalizar', colorBotonCompletado: 'btn-primary' },
+  { id: 3, nombre: 'Estudiar python', completado: 'Finalizar', colorBotonCompletado: 'btn-primary' }
 ]
 
 renderTareas(tareas)
@@ -16,7 +17,7 @@ btnAgregar.addEventListener('click', () => {
   console.log('entra')
   /* Agregamos el invitado al arreglo */
   const nombreTarea = tareaInput.value
-  tareas.push({ id: Date.now(), nombre: nombreTarea, completado: false })
+  tareas.push({ id: Date.now(), nombre: nombreTarea, completado: 'Finalizar', colorBotonCompletado: 'btn-primary'})
   tareaInput.value = ''
   /* Actualizamos la información en el HTML */
   renderTareas(tareas)
@@ -24,31 +25,42 @@ btnAgregar.addEventListener('click', () => {
 
 function borrar (id) {
   const index = tareas.findIndex((ele) => ele.id === id) /* 2.1 */
+  console.log(index)
   tareas.splice(index, 1) /* 2.2 */
+  renderTareas(tareas)
+}
+
+function finalizarTarea (id) {
+  const index = tareas.findIndex((ele) => ele.id === id) /* 2.1 */
+  console.log(index)
+  tareas[index].completado = 'Finalizada'
+  tareas[index].colorBotonCompletado = 'btn-success'
   renderTareas(tareas)
 }
 
 /* Actualizamos la información en el HTML */
 function renderTareas (objetoTareas) {
   let html = ''
+  let contadorTareasFinalizadas = 0
+
   for (const value of objetoTareas) {
+    if (value.completado === 'Finalizada') {
+      contadorTareasFinalizadas += 1
+    }
+
     html += `<ul class="list-group list-group-horizontal row">
               <li class="list-group-item col">${value.id} </li>
               <li class="list-group-item col">${value.nombre} </li>
               <li class="list-group-item col"> 
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label" for="flexCheckDefault">
-                      Tarea terminada
-                      ${value.completado}
-                    </label>
-                  </div>
+              <button onclick="finalizarTarea(${value.id})" class="btn ${value.colorBotonCompletado}" type="button" id="button-finalizar">${value.completado}</button>
               </li>
               <li class="list-group-item col">
-               <button onclick="borrar(${value.id})" class="btn btn-danger" type="button" id="button-addon2">Eliminar</button>
+               <button onclick="borrar(${value.id})" class="btn btn-danger" type="button" id="button-borrar">Eliminar</button>
               </li>
             </ul>`
   }
   listaDeTareas.innerHTML = html
   cuentaTareas.innerHTML = objetoTareas.length
+  idTareasTerminadas.innerHTML = contadorTareasFinalizadas
+  console.log(tareas)
 }
